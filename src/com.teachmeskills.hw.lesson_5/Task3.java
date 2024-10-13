65908d4d64ddf98e4b20fbd83a110bf07b02f2f5
Marharyta Amosava <mozheliukrita@gmail.com>
2024-10-13 14:23:34 +0200

    TMS Task3
    1) summArrayElements variable was renamed to more readable

--- src/com.teachmeskills.hw.lesson_5/Task3.java
+++ src/com.teachmeskills.hw.lesson_5/Task3.java
@@ -20,18 +20,18 @@ public class Task3 {
         int userInputY = input.nextInt();
 
         int[][] twoDimensionalArray = new int[userInputX][userInputY];
-        int summOfArray = 0;
+        int summArrayElements = 0;
 
         System.out.println("Created array: ");
         for (int i = 0; i < twoDimensionalArray.length; i++) {
             for (int j = 0; j < twoDimensionalArray[i].length; j++) {
                 twoDimensionalArray[i][j] = (int) (Math.random() * 10);
                 System.out.print((twoDimensionalArray[i][j]) + " ");
-                summOfArray += twoDimensionalArray[i][j];
+                summArrayElements += twoDimensionalArray[i][j];
             }
             System.out.println();
         }
 
-        System.out.println("The array sum is: " + summOfArray);
+        System.out.println("The array sum is: " + summArrayElements);
     }
 }
