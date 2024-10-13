341a493e78a14464bbd7df9bad23306c6e27bc98
Marharyta Amosava <mozheliukrita@gmail.com>
2024-10-13 13:58:01 +0200

    TMS Task2
    1) Task 2 is completed - multiplication of two arrays

--- src/com.teachmeskills.hw.lesson_5/Task2.java
+++ src/com.teachmeskills.hw.lesson_5/Task2.java
@@ -2,7 +2,7 @@ package com.teachmeskills.hw.lesson_5;
 
 /**
  * 2*. Умножение двух матриц
- * Создайте два массива целых чисел размером 3х3 (две матрицы).
+ * Создайте два массива целых чисел (две матрицы).
  * Напишите программу для умножения двух матриц.
  * Первый массив: {{1,0,0,0},{0,1,0,0},{0,0,0,0}}
  * Второй массив: {{1,2,3},{1,1,1},{0,0,0},{2,1,0}}
@@ -13,5 +13,23 @@ public class Task2 {
 
     public static void Task2Runner(){
 
+        int[][] firstArrayToMultiply =  {{1,0,0,0},{0,1,0,0},{0,0,0,0}};
+        int[][] secondArrayToMultiply = {{1,2,3},{1,1,1},{0,0,0},{2,1,0}};
+        int[][] resultOfMultiplication = new int[3][3];
+
+        for (int i = 0; i < firstArrayToMultiply.length; i++) {
+            for (int j = 0; j < secondArrayToMultiply[0].length; j++) {
+                for (int k = 0; k < secondArrayToMultiply.length; k++) {
+                    resultOfMultiplication[i][j] += firstArrayToMultiply[i][k] * secondArrayToMultiply[k][j];
+                }
+            }
+        }
+
+        System.out.print("The result of multiplication: ");
+        for (int i = 0; i < resultOfMultiplication.length; i++) {
+            for (int j = 0; j < resultOfMultiplication[i].length; j++) {
+                System.out.print((resultOfMultiplication[i][j]) + " ");
+            }
+        }
     }
 }
